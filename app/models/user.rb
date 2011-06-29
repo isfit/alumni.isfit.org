e040691267cff99797c54b73a50040c2091efb7e
Stian Fredrikstad <stianfr@isfit.org>
2011-06-30 01:45:47 +0200

    Sikkert ikke viktig

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 #  has_and_belongs_to_many :roles, :join_table => 'users_roles'
-  has_many :users_roles
-  has_many :roles, :through => :users_roles, :source => :role
+#  has_many :users_roles
+#  has_many :roles, :through => :users_roles, :source => :role
+has_and_belongs_to_many :roles
 #  has_and_belongs_to_many :roles
 end
